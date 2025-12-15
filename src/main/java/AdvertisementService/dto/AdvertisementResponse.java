f9303812420826054cd0dc0766a03db918c6532f
Nezhdanchik1 <myktybekov1234@gmail.com>
2025-12-15 20:29:04 +0500

    Getter setter for AdvResponse

--- src/main/java/AdvertisementService/dto/AdvertisementResponse.java
+++ src/main/java/AdvertisementService/dto/AdvertisementResponse.java
@@ -1,9 +1,17 @@
 package AdvertisementService.dto;
 
 import AdvertisementService.model.AdvertisementStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdvertisementResponse {
    private UUID id;
    private String title;
